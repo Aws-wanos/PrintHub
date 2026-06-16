b8c11fa7d24f60df1b13fbc3df7e089bf8113402
Aws-wanos <aws.wanos@gmail.com>
2026-06-16 20:16:18 +0300

    Fix SEO.js: add ogImage default value

--- shop-frontend/src/components/SEO.js
+++ shop-frontend/src/components/SEO.js
@@ -2,15 +2,15 @@ import React from "react";
 import { Helmet } from "react-helmet";
 
 function SEO({
-  title = "PrintHub Moscow - Печать на футболках, кружках, шоперах и подушках в Москве",
-  description = "Качественная печать на футболках, кружках, шоперах и подушках в Москве. Индивидуальный дизайн. Быстрая доставка.",
-  keywords = "печать на футболках Москва, печать на кружках Москва, печать на шоперах Москва, печать на подушках Москва, PrintHub Moscow",
-  ogUrl = "https://printhubmoscow.ru",
-  author = "PrintHub Moscow",
+  title = "PrintHub - Качественная печать на одежде и аксессуарах",
+  description = "Качественная печать на футболках, кружках, шоперах и подушках в Москве. Индивидуальный дизайн. Быстрая доставка по России. Закажите сейчас!",
+  keywords = "печать на футболках, печать на кружках, печать на шоперах, печать на подушках, сувенирная продукция, подарки на заказ, москва, россия, printhub",
+  ogImage = "/logo192.png",
+  ogUrl = "https://aws-wanos-printhub-d2ce.twc1.net",
+  author = "PrintHub",
 }) {
   return (
     <Helmet>
-      {/* Basic Meta Tags */}
       <html lang="ru" />
       <title>{title}</title>
       <meta name="description" content={description} />
@@ -18,39 +18,29 @@ function SEO({
       <meta name="author" content={author} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="robots" content="index, follow" />
-      <meta name="language" content="Russian" />
 
-      {/* Google / Search Engine Tags */}
       <meta itemProp="name" content={title} />
       <meta itemProp="description" content={description} />
       <meta itemProp="image" content={ogImage} />
 
-      {/* Facebook / Open Graph Tags */}
       <meta property="og:type" content="website" />
       <meta property="og:url" content={ogUrl} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={ogImage} />
-      <meta property="og:site_name" content="Мой Магазин" />
+      <meta property="og:site_name" content="PrintHub" />
       <meta property="og:locale" content="ru_RU" />
 
-      {/* Twitter Card Tags */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={ogImage} />
 
-      {/* Canonical URL */}
       <link rel="canonical" href={ogUrl} />
-
-      {/* Alternate language versions */}
       <link rel="alternate" href={ogUrl} hrefLang="ru" />
 
-      {/* Mobile optimization */}
       <meta name="format-detection" content="telephone=no" />
       <meta name="HandheldFriendly" content="true" />
-
-      {/* Theme color for browsers */}
       <meta name="theme-color" content="#4CAF50" />
     </Helmet>
   );
